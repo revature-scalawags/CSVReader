28601cd8e9f9bbff3d93376b11444f2f05bb48de
Cody Piazza <codypiazza@gmail.com>
2020-12-15 11:26:20 -0500

    refactor to fileReader class with method

--- src/main/scala/csvreader/CSVReader.scala
+++ src/main/scala/csvreader/CSVReader.scala
@@ -10,19 +10,6 @@ import scala.util.{Failure, Success}
   * 
   */
 object CSVReader extends App {
-    val file = io.Source.fromFile("people.csv") 
-    var m = Map[String, Int]()
-    for (line <- file.getLines) {
-      var state = line.split(",")(2)
-      if (m.contains(state)) {
-        m(state) += 1
-      } else {
-        m(state) = 1
-      }
-    }
-    m.foreach(println)
-
-    val future = Future {
-      m.foreach(println)
-    } 
+    val stateMap = new fileReader("people.csv")
+    stateMap.countStates()
 }
